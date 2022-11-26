0712c46b71fa160a0d9eb0fad8aaf6242a914d2d
Mykola Rodin <mykola@nova-labs.com>
2022-11-26 14:12:15 +0200

    Update q1 test

--- q1/q1task_test.go
+++ q1/q1task_test.go
@@ -3,33 +3,37 @@ package q1
 import "testing"
 
 func Test_SortWords(t *testing.T) {
-	inputs := [][]string{
-		{"aaaasd", "a", "aab", "aaabcd", "ef", "cssssssd", "fdz", "kf", "zc", "lklklklklklklklkl", "l"},
-		{"aab", "aba", "aaa"},
-		{"b"},
-		{"a"},
-		{""},
-		{},
-	}
-	outputs := [][]string{
-		{"aaaasd", "aaabcd", "aab", "a", "lklklklklklklklkl", "cssssssd", "fdz", "ef", "kf", "zc", "l"},
-		{"aaa", "aab", "aba"},
-		{"b"},
-		{"a"},
-		{""},
-		{},
+	testCases := []struct {
+		given    []string
+		expected []string
+	}{
+		{
+			given:    []string{"aaaasd", "a", "aab", "aaabcd", "ef", "cssssssd", "fdz", "kf", "zc", "lklklklklklklklkl", "l"},
+			expected: []string{"aaaasd", "aaabcd", "aab", "a", "lklklklklklklklkl", "cssssssd", "fdz", "ef", "kf", "zc", "l"},
+		}, {
+			given:    []string{"aab", "aba", "aaa"},
+			expected: []string{"aaa", "aab", "aba"},
+		}, {
+			given:    []string{"b"},
+			expected: []string{"b"},
+		}, {
+			given:    []string{"a"},
+			expected: []string{"a"},
+		}, {
+			given:    []string{""},
+			expected: []string{""},
+		}, {
+			given:    []string{},
+			expected: []string{},
+		},
 	}
 
-	testPosLen := len(inputs)
-	for testPos := 0; testPos < testPosLen; testPos++ {
-		given := inputs[testPos]
-		actual := SortWords(given)
-		expected := outputs[testPos]
+	for _, testCase := range testCases {
+		actual := SortWords(testCase.given)
 
-		givenLen := len(given)
-		for i := 0; i < givenLen; i++ {
-			if actual[i] != expected[i] {
-				t.Errorf("sortWords(%v) returned:\n%v;\nwant:\n%v", given, actual, expected)
+		for i := 0; i < len(testCase.given); i++ {
+			if actual[i] != testCase.expected[i] {
+				t.Errorf("sortWords(%v) returned:\n%v;\nwant:\n%v", testCase.given, actual, testCase.expected)
 			}
 		}
 	}
